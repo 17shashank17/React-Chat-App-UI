5f4428ba4c4b8c406804f353d4b6a88db5c3184a
17shashank17 <17shashank17@gmail.com>
2020-06-03 10:24:46 +0530

    UI added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import "@progress/kendo-theme-default/dist/all.css";
+import { Chat } from "@progress/kendo-react-conversational-ui";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.user = {
+            id: 1,
+            avatarUrl: "https://via.placeholder.com/24/008000/008000.png"
+        };
+        this.bot = { id: 0 };
+        this.state = {
+            messages: [
+                {
+                    author: this.bot,
+                    timestamp: new Date(),
+                    text: "Hello, Welcome to Walmart MarketPlace!"
+                }
+            ]
+        };
+    }
+
+    addNewMessage = (event) => {
+        let botResponse = Object.assign({}, event.message);
+        botResponse.text = "hello";
+        botResponse.author = this.bot;
+        this.setState((prevState) => ({
+            messages: [
+                ...prevState.messages,
+                event.message
+            ]
+        }));
+        this.setState(prevState => ({
+            messages: [
+                ...prevState.messages,
+                botResponse
+            ]
+        }));
+    };
+
+
+    render() {
+        return (
+            <div className = "appContainer">
+                <Chat user={this.user}
+                    messages={this.state.messages}
+                    onMessageSend={this.addNewMessage}
+                    placeholder={"Type a message..."}
+                    width={400}>
+                </Chat>
+            </div>
+        );
+    }
 }
 
-export default App;
